d4b13feca4c55dd12972ec63c85bd4454a4bbdf9
endormi <ernosalo14@gmail.com>
2020-11-12 16:03:47 +0200

    Update sys/clean.sh

--- sys/clean.sh
+++ sys/clean.sh
@@ -3,31 +3,35 @@
 python3 -c "from logos import Logo; Logo('Clean');"
 
 
+echo "Truncate logs:"
+
 if [[ "$EUID" -eq 0 ]]; then
-  # WIP
-  #log=/var/log
-  #cd $log
-  #for i in $(find $log -type f); do cat /dev/null > $i; done
-  echo "This part of the code is WIP\n"
+  log=/var/log
+  cd $log
+  echo Truncating logs..
+  sleep 1
+  for i in $(find $log -type f); do cat /dev/null > $i; done
+  echo Done!
 else
-  #echo "Unable to truncate logs."
-  echo -e "You are not root.\n"
+  echo "Unable to truncate logs."
+  echo -e "You are not root.\n\n"
 fi
 
 trash=~/.local/share/Trash/*
+echo "Empty the trash:"
 choices="1 or all"
 echo "choices: $choices (1 means going through files one by one and all means, well you know..) "
 read -p "Type in your choice: " choice
 choice="$(echo ${choice} | tr 'A-Z' 'a-z')"
 
 case "$choice" in
-  "1" | "-1")
+  "1")
     rm -rf -i $trash
     ;;
-  "all" | "-a" | "a")
-    rm -rf $trash
-    echo Emptying the entire trash
+  "2" | "all" | "-a" | "--a" | "a")
+    echo Emptying the trash..
     sleep 1
+    rm -rf $trash
     echo Done!
     ;;
   *)

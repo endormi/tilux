ed11c60f82511532bda9d97c90de012117cea8b2
endormi <ernosalo14@gmail.com>
2020-12-28 16:46:40 +0200

    Decrease sleep for info

--- info.rb
+++ info.rb
@@ -19,19 +19,19 @@ RubyPython.stop
 
 os = RbConfig::CONFIG["host_os"]
 puts "OS: #{os}"
-sleep 1
+sleep 0.5
 host = Socket.gethostname
 puts "Hostname: #{host}"
-sleep 1
+sleep 0.5
 
 # description of Linux distro
 print `lsb_release -d`
-sleep 1
+sleep 0.5
 # codename
 print `lsb_release -c`
-sleep 1
+sleep 0.5
 print "Kernel version: ", `uname -srm`
-sleep 1
+sleep 0.5
 
 
 # Should work with ruby version 1.8.6+
@@ -43,14 +43,14 @@ begin
   # I was unable to get public ip from socket
   ip = `dig +short myip.opendns.com @resolver1.opendns.com`
   puts "Your public IP: #{ip}"
-  sleep 1
+  sleep 0.5
   # another way to get public IP
   #resp = Net::HTTP.get URI.parse('http://checkip.dyndns.org')
   #puts "Your IP: #{resp.match( /(?:Address: )([\d\.]+)/)[1]}"
 
   p_ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
   puts "Your private IP: #{p_ip}"
-  sleep 1
+  sleep 0.5
 rescue Resolv::ResolvError => e
   puts "\nNo internet connection!\nUnable to get IP."
   # needed because \n (new line) doesn't work when using print
@@ -59,5 +59,5 @@ end
 
 
 print "Python3 version: ", `python3 -V`
-sleep 1
+sleep 0.5
 print "Ruby version: ", `ruby -v`, "\n"
--- spec.c
+++ spec.c
@@ -2,4 +2,6 @@
 #include<stdlib.h>
 
 int main()
-{}
+{
+  
+}

ad7cbdfcd918d695e5db515b37562cfed6935321
endormi <ernosalo14@gmail.com>
2021-01-12 18:32:59 +0200

    Add disclaimer

--- networking/ip_pinging.py
+++ networking/ip_pinging.py
@@ -13,6 +13,12 @@ time.sleep(1)
 
 file = open('/tmp/ip_ping_results.txt', 'w')
 
+print("NOTE:")
+print("While this script is legal, this still can be used maliciously.")
+print("Don't use this code maliciously!")
+print("This is meant for good purposes, DO NOT use this maliciously. DO NOT be a criminal.\n")
+print("Only PING IPs that belong to you and the people you have gained permission from!\n")
+
 net = input('Enter network e.g. [192.168.1.1]: ')[0:-1]
 start = input('Starting IP: ')
 end = input('Ending IP: ')

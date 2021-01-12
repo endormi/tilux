893cf32ac15d6bdf6e62ebf6bba1541cedfc451d
Erno Salo <39559256+endormi@users.noreply.github.com>
2021-01-12 18:38:58 +0200

    Update ip_pinging.py

--- networking/ip_pinging.py
+++ networking/ip_pinging.py
@@ -14,9 +14,9 @@ time.sleep(1)
 file = open('/tmp/ip_ping_results.txt', 'w')
 
 print("NOTE:")
-print("While this script is legal, this still can be used maliciously.")
-print("Don't use this code maliciously!")
-print("This is meant for good purposes, DO NOT use this maliciously. DO NOT be a criminal.\n")
+print("While this script is legal, this still can be used with malicious intent.")
+print("Don't use this code with malicious intent!")
+print("This is meant for good purposes, DO NOT use this with malicious intent. DO NOT be a criminal.\n")
 print("Only PING IPs that belong to you and the people you have gained permission from!\n")
 
 net = input('Enter network e.g. [192.168.1.1]: ')[0:-1]

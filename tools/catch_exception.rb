2917a015d888181bbf7d53153a91261dfa7e0566
endormi <ernosalo14@gmail.com>
2021-02-04 19:02:56 +0200

    Update tools/catch_exception.rb

--- tools/catch_exception.rb
+++ tools/catch_exception.rb
@@ -8,8 +8,8 @@ end
 
 # Checks if input is empty
 def empty_input?(choice)
-  if choice == ''
-    puts "Choice can't be empty!"
-    exit
-  end
+  return unless choice.empty?
+
+  puts "Choice can't be empty"
+  exit
 end

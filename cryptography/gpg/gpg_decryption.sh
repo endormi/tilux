9345d0a6c4df0b6ffac643b6b62024eeb40e4085
endormi <ernosalo14@gmail.com>
2021-04-10 23:34:40 +0300

    Add the initial functionality for catching empty inputs

--- cryptography/gpg/gpg_decryption.sh
+++ cryptography/gpg/gpg_decryption.sh
@@ -4,10 +4,13 @@
 # Still a WIP code
 #
 
+source ./tools/catch
 python3 -c "from tools.logos import Logo; Logo('GPG Decryption');"
 sleep 1
 
 read -p "What is the filename you want to decrypt? " filename
+catch_empty $filename
 read -p "Add the filename including the extension: " f
+catch_empty $f
 gpg --decrypt $filename > $f
 less $f
--- sys/bf.sh
+++ sys/bf.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+source ./tools/catch
 python3 -c "from tools.logos import Logo; Logo('BF');"
 
 bu=`date +%d-%m-%y`
 read -p "Type in the folder you want to backup: " src
+catch_empty $src
 read -p "Type in the destination: " dest
+catch_empty $dest
 
 tar -cpzf $dest-$bu.tar.gz $src

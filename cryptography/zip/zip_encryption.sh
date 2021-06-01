15992be7c5e7e5e18224089423062027e521bb45
endormi <ernosalo14@gmail.com>
2021-06-01 16:22:08 +0300

    Move and clear pkg installation before logo in scripts

--- cryptography/zip/zip_encryption.sh
+++ cryptography/zip/zip_encryption.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+if [[ "$(which zip)" == "" ]]; then
+  sudo apt upgrade
+  sudo apt install zip
+  clear
+fi
+
 if [[ $1 == "tilux" ]]; then
   source ./tools/catch
   python3 -c "from tools.logos import Logo; Logo('Zip Encryption');"
 fi
 sleep 1
 
-if [[ "$(which zip)" == "" ]]; then
-  sudo apt-get upgrade
-  sudo apt install zip
-fi
 
 echo -e "Warning! The standard ZIP encryption is very weak.\n"
 read -p "What file(s) do you want to encrypt? " f

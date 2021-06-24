2ad2b03b979f4770b6a7ed5af4aa48e8a8374704
endormi <ernosalo14@gmail.com>
2021-06-24 21:03:46 +0300

    Check for glxinfo

--- sys/gpu_name.sh
+++ sys/gpu_name.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$(which glxinfo)" == "" ]]; then
+  sudo apt update
+  sudo apt install mesa-utils
+  clear
+fi
+
 if [[ $1 == "tilux" ]]; then python3 -c "from tools.logos import Logo; Logo('GPU name');"; fi
 
 sleep .5

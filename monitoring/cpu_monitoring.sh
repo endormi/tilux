70d41f9511dbc894a8860645e39d6b1929e60a17
endormi <ernosalo14@gmail.com>
2021-06-30 14:23:59 +0300

    Update terminal output in CPU and memory monitoring
    Terminal output changes depending if you open up the file or not. Output needs to be prettified.

--- monitoring/cpu_monitoring.sh
+++ monitoring/cpu_monitoring.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# TODO: Terminal output
+
 secs=3000
 time=10
 cproc=$(( $secs / $time ))
 
 function cpu {
-  echo
-  echo
-  echo CPU usage:
+  echo -e "\nCPU usage (biggest resource drainers):"
   cat /tmp/cpu_usage.$$ | sort -nrk 2 | head
 
   rm /tmp/cpu_usage.$$
-  exit 0
+  exit
 }
 
 if [[ $1 == "tilux" ]]; then python3 -c "from tools.logos import Logo; Logo('CPU monitoring');"; fi
@@ -20,6 +20,8 @@ sleep 1
 trap 'cpu' INT
 
 echo Watching CPU usage...
+echo -e "Check /tmp/cpu_usage.$$ \n"
+echo "Pressing CTRL-C will display the biggest resource drainers sorted."
 
 for((i=0; i<$cproc; i++)); do
     ps -eocomm,pcpu | egrep -v '(0.0)|(%CPU)' >> /tmp/cpu_usage.$$
--- monitoring/mem_monitoring.sh
+++ monitoring/mem_monitoring.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# TODO: Terminal output
+
 secs=3000
 time=10
 mproc=$(( $secs / $time ))
 
 function mem {
-  echo
-  echo
-  echo Memory usage:
+  echo -e "\nMemory usage (biggest resource drainers):"
   cat /tmp/memory_usage.$$ | sort -nrk 2 | head
 
   rm /tmp/memory_usage.$$
-  exit 0
+  exit
 }
 
 if [[ $1 == "tilux" ]]; then python3 -c "from tools.logos import Logo; Logo('Mem monitoring');"; fi
@@ -20,6 +20,8 @@ sleep 1
 trap 'mem' INT
 
 echo Watching Memory usage...
+echo -e "Check /tmp/memory_usage.$$ \n"
+echo "Pressing CTRL-C will display the biggest resource drainers sorted."
 
 for((i=0; i<$mproc; i++)); do
     ps -eocomm,pmem | egrep -v '(0.0)|(%MEM)' >> /tmp/memory_usage.$$

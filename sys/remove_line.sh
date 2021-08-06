8bc9244f602834e7184c0816f445bfea59c45274
endormi <ernosalo14@gmail.com>
2021-08-06 23:41:06 +0300

    Remove extra line

--- sys/remove_line.sh
+++ sys/remove_line.sh
@@ -2,7 +2,6 @@
 
 if [[ $1 == "tilux" ]]; then python3 -c "from tools.logos import Logo; Logo('Remove line');"; fi
 
-
 read -p "Remove line (matching the pattern): " pattern
 read -p "Filename (to remove the line from): " filename
 

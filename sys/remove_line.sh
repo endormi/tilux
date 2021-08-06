c25bd7191a48a026e032eb5e829def350ac191ae
endormi <ernosalo14@gmail.com>
2021-08-07 00:34:30 +0300

    Add catch_empty to remove_line

--- sys/remove_line.sh
+++ sys/remove_line.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [[ $1 == "tilux" ]]; then python3 -c "from tools.logos import Logo; Logo('Remove line');"; fi
+if [[ $1 == "tilux" ]]; then
+  source ./tools/catch
+  python3 -c "from tools.logos import Logo; Logo('Remove line');"
+fi
 
 read -p "Remove line (matching the pattern): " pattern
+if [[ $1 == "tilux" ]]; then catch_empty $pattern; fi
+
 read -p "Filename (to remove the line from): " filename
+if [[ $1 == "tilux" ]]; then catch_empty $filename; fi
 
 echo "$(grep -v "$pattern" $filename)" > $filename

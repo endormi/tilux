52a25b9530f1c2a33b6c6914e0326ece04724362
endormi <ernosalo14@gmail.com>
2021-08-09 16:17:07 +0300

    One line when statements and convert args to lowercase

--- tools/def.rb
+++ tools/def.rb
@@ -7,10 +7,8 @@
 ### Cryptography options
 def ccrypt_options(choice)
   case choice
-  when '1', '-e', '--e'
-    sys('./cryptography/ccrypt/ccrypt_encryption.sh tilux')
-  when '2', '-d', '--d'
-    sys('./cryptography/ccrypt/ccrypt_decryption.sh tilux')
+  when '-e', '--e' then sys('./cryptography/ccrypt/ccrypt_encryption.sh tilux')
+  when '-d', '--d' then sys('./cryptography/ccrypt/ccrypt_decryption.sh tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -19,10 +17,8 @@ end
 
 def gpg_options(choice)
   case choice
-  when '1', '-e', '--e'
-    sys('./cryptography/gpg/gpg_encryption.sh tilux')
-  when '2', '-d', '--d'
-    sys('./cryptography/gpg/gpg_decryption.sh tilux')
+  when '-e', '--e' then sys('./cryptography/gpg/gpg_encryption.sh tilux')
+  when '-d', '--d' then sys('./cryptography/gpg/gpg_decryption.sh tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -31,10 +27,8 @@ end
 
 def openssl_options(choice)
   case choice
-  when '1', '-e', '--e'
-    sys('./cryptography/openssl/openssl_encryption.sh tilux')
-  when '2', '-d', '--d'
-    sys('./cryptography/openssl/openssl_decryption.sh tilux')
+  when '-e', '--e' then sys('./cryptography/openssl/openssl_encryption.sh tilux')
+  when '-d', '--d' then sys('./cryptography/openssl/openssl_decryption.sh tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -43,10 +37,8 @@ end
 
 def pyca_options(choice)
   case choice
-  when '1', '-e', '--e'
-    sys('./cryptography/pyca/encrypt.py tilux')
-  when '2', '-d', '--d'
-    sys('./cryptography/pyca/decrypt.py tilux')
+  when '-e', '--e' then sys('./cryptography/pyca/encrypt.py tilux')
+  when '-d', '--d' then sys('./cryptography/pyca/decrypt.py tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -55,10 +47,8 @@ end
 
 def zip_options(choice)
   case choice
-  when '1', '-e', '--e'
-    sys('./cryptography/zip/zip_encryption.sh tilux')
-  when '2', '-d', '--d'
-    sys('./cryptography/zip/zip_decryption.sh tilux')
+  when '-e', '--e' then sys('./cryptography/zip/zip_encryption.sh tilux')
+  when '-d', '--d' then sys('./cryptography/zip/zip_decryption.sh tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -75,15 +65,9 @@ end
 
 def monitoring_options(choice)
   case choice
-  when '1', '-c', '--c'
-    system('clear')
-    system('./monitoring/cpu_monitoring.sh tilux')
-  when '2', '-m', '--m'
-    system('clear')
-    system('./monitoring/mem_monitoring.sh tilux')
-  when '3', '-w', '--w'
-    system('clear')
-    system('./monitoring/website_monitoring.py tilux')
+  when '-c', '--c' then system('clear'); system('./monitoring/cpu_monitoring.sh tilux')
+  when '-m', '--m' then system('clear'); system('./monitoring/mem_monitoring.sh tilux')
+  when '-w', '--w' then system('clear'); system('./monitoring/website_monitoring.py tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -92,10 +76,8 @@ end
 
 def networking_options(choice)
   case choice
-  when '1', '-i', '--i'
-    sys('./networking/ip_pinging.py tilux')
-  when '2', '-s', '--s'
-    sys('./networking/server_pinging.py tilux')
+  when '-i', '--i' then sys('./networking/ip_pinging.py tilux')
+  when '-s', '--s' then sys('./networking/server_pinging.py tilux')
   else
     # TODO
     puts "\nInvalid choice!"
@@ -104,72 +86,41 @@ end
 
 def sys_options(choice)
   case choice
-  when '-a', '--a'
-    sys('./sys/add_alias.sh tilux')
-  when '-bf', '--bf'
-    sys('./sys/bf.sh tilux')
-  when '-b', '--b'
-    sys('./sys/bit.sh tilux')
-  when '-c', '--c'
-    sys('./sys/clean.sh tilux')
-  when '-cdp', '--cdp'
-    sys('./sys/convert_doc_to_pdf.rb tilux')
-  when '-ci', '--ci'
-    sys('./sys/convert_img.rb tilux')
-  when '-cpu', '--cpu'
-    sys('./sys/cpu_name.sh tilux')
-  when '-d', '--d'
-    sys('./sys/del.rb tilux')
-  when '-di', '--di'
-    sys('./sys/distro.sh tilux')
-  when '-e', '--e'
-    sys('./sys/exists.rb tilux')
-  when '-f', '--f'
-    sys('./sys/fd.rb tilux')
-  when '-fl', '--fl'
-    sys('./sys/file_updated.rb tilux')
-  when '-fp', '--fp'
-    sys('./sys/force_poweroff.sh tilux')
-  when '-fr', '--fr'
-    sys('./sys/force_reboot.sh tilux')
-  when '-fs', '--fs'
+  when '-a', '--a' then sys('./sys/add_alias.sh tilux')
+  when '-bf', '--bf' then sys('./sys/bf.sh tilux')
+  when '-b', '--b' then sys('./sys/bit.sh tilux')
+  when '-c', '--c' then sys('./sys/clean.sh tilux')
+  when '-cdp', '--cdp' then sys('./sys/convert_doc_to_pdf.rb tilux')
+  when '-ci', '--ci' then sys('./sys/convert_img.rb tilux')
+  when '-cpu', '--cpu' then sys('./sys/cpu_name.sh tilux')
+  when '-d', '--d' then sys('./sys/del.rb tilux')
+  when '-di', '--di' then sys('./sys/distro.sh tilux')
+  when '-e', '--e' then sys('./sys/exists.rb tilux')
+  when '-f', '--f' then sys('./sys/fd.rb tilux')
+  when '-fl', '--fl' then sys('./sys/file_updated.rb tilux')
+  when '-fp', '--fp' then sys('./sys/force_poweroff.sh tilux')
+  when '-fr', '--fr' then sys('./sys/force_reboot.sh tilux')
+  when '-fs', '--fs' then
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('FS');"`
     system('./fs.o')
-  when '-g', '--g'
-    sys('./sys/git_config.sh tilux')
-  when '-gtd', '--gtd'
-    sys('./sys/gnome_term_profile_dump.sh tilux')
-  when '-gtl', '--gtl'
-    sys('./sys/gnome_term_profile_load.sh tilux')
-  when '-gpu', '--gpu'
-    sys('./sys/gpu_name.sh tilux')
-  when '-h', '--h'
-    sys('./sys/hostname.rb tilux')
-  when '-img', '--img'
-    sys('./sys/img_info.rb tilux')
-  when '-i', '--i'
-    sys('./sys/ip.rb tilux')
-  when '-k', '--k'
-    sys('./sys/kernel_version.sh tilux')
-  when '-l', '--l'
-    sys('./sys/last_updated.sh tilux')
-  when '-o', '--o'
-    sys('./sys/os.rb tilux')
-  when '-p', '--p'
-    sys('./sys/parse_date.rb tilux')
-  when '-prv', '--prv'
-    sys('./sys/prv.sh tilux')
-  when '-rl', '--rl'
-    sys('./sys/remove_line.sh tilux')
-  when '-ri', '--ri'
-    sys('./sys/resize_img.rb tilux')
-  when '-rt', '--rt'
-    sys('./sys/resize_terminal.sh tilux')
-  when '-r', '--r'
-    sys('./sys/root.rb tilux')
-  when '-ro', '--ro'
-    sys('./sys/rotate_img.rb tilux')
+  when '-g', '--g' then sys('./sys/git_config.sh tilux')
+  when '-gtd', '--gtd' then sys('./sys/gnome_term_profile_dump.sh tilux')
+  when '-gtl', '--gtl' then sys('./sys/gnome_term_profile_load.sh tilux')
+  when '-gpu', '--gpu' then sys('./sys/gpu_name.sh tilux')
+  when '-h', '--h' then sys('./sys/hostname.rb tilux')
+  when '-img', '--img' then sys('./sys/img_info.rb tilux')
+  when '-i', '--i' then sys('./sys/ip.rb tilux')
+  when '-k', '--k' then sys('./sys/kernel_version.sh tilux')
+  when '-l', '--l' then sys('./sys/last_updated.sh tilux')
+  when '-o', '--o' then sys('./sys/os.rb tilux')
+  when '-p', '--p' then sys('./sys/parse_date.rb tilux')
+  when '-prv', '--prv' then sys('./sys/prv.sh tilux')
+  when '-rl', '--rl' then sys('./sys/remove_line.sh tilux')
+  when '-ri', '--ri' then sys('./sys/resize_img.rb tilux')
+  when '-rt', '--rt' then sys('./sys/resize_terminal.sh tilux')
+  when '-r', '--r' then sys('./sys/root.rb tilux')
+  when '-ro', '--ro' then sys('./sys/rotate_img.rb tilux')
   when '-s', '--s'
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('Space');"`

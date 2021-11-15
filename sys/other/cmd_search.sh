9133a116cfd422628409b5001221bbb78f6f8395
endormi <ernosalo14@gmail.com>
2021-11-15 10:46:38 +0200

    Add catch_empty to cmd_search

--- sys/other/cmd_search.sh
+++ sys/other/cmd_search.sh
@@ -5,6 +5,7 @@ if [[ $1 == "tilux" ]]; then
   python3 -c "from tools.logos import Logo; Logo('CMD Search');"
 fi
 
-
 read -p "Search: " search
+if [[ $1 == "tilux" ]]; then catch_empty $search; fi
+
 compgen -caX "!*$search*"

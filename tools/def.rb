5d8b6df0aea67d450dea8fb460959b86f798fae3
endormi <ernosalo14@gmail.com>
2022-04-18 18:05:58 +0300

    Add hash for commands to run

--- tools/def.rb
+++ tools/def.rb
@@ -5,189 +5,137 @@
 # and then to make it functional, using sys_options(input)
 
 ### Cryptography options
-def ccrypt_options(choice)
-  case choice
-  when '-e', '--e' then sys('./cryptography/ccrypt/ccrypt_encryption.sh tilux')
-  when '-d', '--d' then sys('./cryptography/ccrypt/ccrypt_decryption.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
-
-def gpg_options(choice)
-  case choice
-  when '-e', '--e' then sys('./cryptography/gpg/gpg_encryption.sh tilux')
-  when '-d', '--d' then sys('./cryptography/gpg/gpg_decryption.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
-
-def openssl_options(choice)
-  case choice
-  when '-e', '--e' then sys('./cryptography/openssl/openssl_encryption.sh tilux')
-  when '-d', '--d' then sys('./cryptography/openssl/openssl_decryption.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
-
-def pyca_options(choice)
-  case choice
-  when '-e', '--e' then sys('./cryptography/pyca/encrypt.py tilux')
-  when '-d', '--d' then sys('./cryptography/pyca/decrypt.py tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
-
-def zip_options(choice)
-  case choice
-  when '-e', '--e' then sys('./cryptography/zip/zip_encryption.sh tilux')
-  when '-d', '--d' then sys('./cryptography/zip/zip_decryption.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
-
 def cryptography_encryption
   puts '-e: Encryption'
   puts "-d: Decrypt\n"
   puts
 end
 
+$ccrypt_options = {
+  '-e' => -> { sys('./cryptography/ccrypt/ccrypt_encryption.sh tilux') },
+  '-d' => -> { sys('./cryptography/ccrypt/ccrypt_decryption.sh tilux') }
+}
+$ccrypt_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
+
+$gpg_options = {
+  '-e' => -> { sys('./cryptography/gpg/gpg_encryption.sh tilux') },
+  '-d' => -> { sys('./cryptography/gpg/gpg_decryption.sh tilux') }
+}
+$gpg_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
+
+$openssl_options = {
+  '-e' => -> { sys('./cryptography/openssl/openssl_encryption.sh tilux') },
+  '-d' => -> { sys('./cryptography/openssl/openssl_decryption.sh tilux') }
+}
+$openssl_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
+
+$pyca_options = {
+  '-e' => -> { sys('./cryptography/pyca/encrypt.py tilux') },
+  '-d' => -> { sys('./cryptography/pyca/decrypt.py tilux') }
+}
+$pyca_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
+
+$zip_options = {
+  '-e' => -> { sys('./cryptography/zip/zip_encryption.sh tilux') },
+  '-d' => -> { sys('./cryptography/zip/zip_decryption.sh tilux') }
+}
+$zip_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
+
 ### End cryptography
 
-def monitoring_options(choice)
-  case choice
-  when '-c', '--c' then system('clear'); system('./monitoring/cpu_monitoring.sh tilux')
-  when '-m', '--m' then system('clear'); system('./monitoring/mem_monitoring.sh tilux')
-  when '-w', '--w' then system('clear'); system('./monitoring/website_monitoring.py tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$monitoring_options = {
+  '-c' => -> { sys('./monitoring/cpu_monitoring.sh tilux') },
+  '-m' => -> { sys('./monitoring/mem_monitoring.sh tilux') },
+  '-w' => -> { sys('./monitoring/website_monitoring.py tilux') }
+}
+$monitoring_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def networking_options(choice)
-  case choice
-  when '-i', '--i' then sys('./networking/ip_pinging.py tilux')
-  when '-s', '--s' then sys('./networking/server_pinging.py tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$networking_options = {
+  '-i' => -> { sys('./networking/ip_pinging.py tilux') },
+  '-s' => -> { sys('./networking/server_pinging.py tilux') }
+}
+$networking_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def conf(choice)
-  case choice
-  when '-gc', '--gc' then sys('./sys/config/git_config.sh tilux')
-  when '-gtd', '--gtd' then sys('./sys/config/gnome_term_profile_dump.sh tilux')
-  when '-gtl', '--gtl' then sys('./sys/config/gnome_term_profile_load.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$conf_options = {
+  '-gc'  => -> { sys('./sys/config/git_config.sh tilux') },
+  '-gtd' => -> { sys('./sys/config/gnome_term_profile_dump.sh tilux') },
+  '-gtl' => -> { sys('./sys/config/gnome_term_profile_load.sh tilux') }
+}
+$conf_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def ff(choice)
-  case choice
-  when '-bf', '--bf' then sys('./sys/file_folder/bf.sh tilux')
-  when '-c', '--c' then sys('./sys/file_folder/clean.sh tilux')
-  when '-del', '--del' then sys('./sys/file_folder/del.rb tilux')
-  when '-e', '--e' then sys('./sys/file_folder/exists.rb tilux')
-  when '-fd', '--fd' then sys('./sys/file_folder/fd.rb tilux')
-  when '-fl', '--fl' then sys('./sys/file_folder/file_updated.rb tilux')
-  when '-fs', '--fs' then
+$ff_options = {
+  '-bf'  => -> { sys('./sys/file_folder/bf.sh tilux') },
+  '-c'   => -> { sys('./sys/file_folder/clean.sh tilux') },
+  '-del' => -> { sys('./sys/file_folder/del.rb tilux') },
+  '-e'   => -> { sys('./sys/file_folder/exists.rb tilux') },
+  '-fd'  => -> { sys('./sys/file_folder/fd.rb tilux') },
+  '-fl'  => -> { sys('./sys/file_folder/file_updated.rb tilux') },
+  '-fs'  => lambda {
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('FS');"`
     system('./sys/file_folder/fs.o')
-  when '-l', '--l' then sys('./sys/file_folder/last_updated.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+  },
+  '-l' => -> { sys('./sys/file_folder/last_updated.sh tilux') }
+}
+$ff_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def image(choice)
-  case choice
-  when '-ci', '--ci' then sys('./sys/img/convert_img.rb tilux')
-  when '-i', '--i' then sys('./sys/img/img_info.rb tilux')
-  when '-ri', '--ri' then sys('./sys/img/resize_img.rb tilux')
-  when '-ro', '--ro' then sys('./sys/img/rotate_img.rb tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$image_options = {
+  '-ci' => -> { sys('./sys/img/convert_img.rb tilux') },
+  '-i'  => -> { sys('./sys/img/img_info.rb tilux') },
+  '-ri' => -> { sys('./sys/img/resize_img.rb tilux') },
+  '-ro' => -> { sys('./sys/img/rotate_img.rb tilux') }
+}
+$image_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def info(choice)
-  case choice
-  when '-bit', '--bit' then sys('./sys/info/bit.sh tilux')
-  when '-di', '--di' then sys('./sys/info/distro.sh tilux')
-  when '-ip', '--ip' then sys('./sys/info/ip.rb tilux')
-  when '-k', '--k' then sys('./sys/info/kernel_version.sh tilux')
-  when '-os', '--os' then sys('./sys/info/os.rb tilux')
-  when '-prv', '--prv' then sys('./sys/info/prv.sh tilux')
-  when '-r', '--r' then sys('./sys/info/root.rb tilux')
-  when '-s', '--s'
+$info_options = {
+  '-bit' => -> { sys('./sys/info/bit.sh tilux') },
+  '-di'  => -> { sys('./sys/info/distro.sh tilux') },
+  '-ip'  => -> { sys('./sys/info/ip.rb tilux') },
+  '-k'   => -> { sys('./sys/info/kernel_version.sh tilux') },
+  '-os'  => -> { sys('./sys/info/os.rb tilux') },
+  '-prv' => -> { sys('./sys/info/prv.sh tilux') },
+  '-r'   => -> { sys('./sys/info/root.rb tilux') },
+  '-s'   => lambda {
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('Space');"`
     system('./sys/info/space.o')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+  }
+}
+$info_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def proc(choice)
-  case choice
-  when '-cpu', '--cpu' then sys('./sys/proc/cpu_name.sh tilux')
-  when '-gpu', '--gpu' then sys('./sys/proc/gpu_name.sh tilux')
-  when '-oc', '--oc'
+$proc_options = {
+  '-cpu' => -> { sys('./sys/proc/cpu_name.sh tilux') },
+  '-gpu' => -> { sys('./sys/proc/gpu_name.sh tilux') },
+  '-oc'  => lambda {
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('Single core');"`
     sleep(1)
     system('./sys/proc/one_core.o')
-  when '-mc', '--mc'
+  },
+  '-mc' => lambda {
     system('clear')
     print `python3 -c "from tools.logos import Logo; Logo('Multi-core');"`
     sleep(1)
     system('./sys/proc/multi_core.o')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+  }
+}
+$proc_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
-def shutdown(choice)
-  case choice
-  when '-fp', '--fp' then sys('./sys/shutdown/force_poweroff.sh tilux')
-  when '-fr', '--fr' then sys('./sys/shutdown/force_reboot.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$shutdown_options = {
+  '-fp' => -> { sys('./sys/shutdown/force_poweroff.sh tilux') },
+  '-fr' => -> { sys('./sys/shutdown/force_reboot.sh tilux') }
+}
+$shutdown_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }
 
 # Just for now
-def sys_other(choice)
-  case choice
-  when '-a', '--a' then sys('./sys/other/add_alias.sh tilux')
-  when '-cdp', '--cdp' then sys('./sys/other/convert_doc_to_pdf.rb tilux')
-  when '-def', '--def' then sys('./sys/other/default_web_browser.sh tilux')
-  when '-s', '--s' then sys('./sys/other/cmd_search.sh tilux')
-  when '-op', '--op' then sys('./sys/other/open.sh tilux')
-  when '-p', '--p' then sys('./sys/other/parse_date.rb tilux')
-  when '-rl', '--rl' then sys('./sys/other/remove_line.sh tilux')
-  when '-rt', '--rt' then sys('./sys/other/resize_terminal.sh tilux')
-  else
-    # TODO
-    puts "\nInvalid choice!"
-  end
-end
+$sys_other_options = {
+  '-a'   => -> { sys('./sys/other/add_alias.sh tilux') },
+  '-cdp' => -> { sys('./sys/other/convert_doc_to_pdf.rb tilux') },
+  '-def' => -> { sys('./sys/other/default_web_browser.sh tilux') },
+  '-s'   => -> { sys('./sys/other/cmd_search.sh tilux') },
+  '-op'  => -> { sys('./sys/other/open.sh tilux') },
+  '-p'   => -> { sys('./sys/other/parse_date.rb tilux') },
+  '-rl'  => -> { sys('./sys/other/remove_line.sh tilux') },
+  '-rt'  => -> { sys('./sys/other/resize_terminal.sh tilux') }
+}
+$sys_other_options.default_proc = ->(_h, k) { raise KeyError, "#{k} Invalid choice!" }

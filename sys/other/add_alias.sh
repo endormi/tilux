835d9fa72543cec82ed6a84307ec7ae2f8e8c0ce
endormi <39559256+endormi@users.noreply.github.com>
2023-06-06 17:41:21 +0300

    Keep consistency with add_alias script

--- sys/other/add_alias.sh
+++ sys/other/add_alias.sh
@@ -12,7 +12,10 @@ fi
 read -p "Alias: " alias
 [ "$1" == "tilux" ] && catch_empty $alias
 
-[ `alias | grep "alias $alias" | wc -l` != 0 ] && echo "Alias $alias already exists" && exit
+if alias $alias &>/dev/null; then
+  echo "Alias $alias already exists!"
+  exit
+fi
 
 read -p "Command: " cmd
 [ "$1" == "tilux" ] && catch_empty $cmd

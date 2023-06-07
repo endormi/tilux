7a6e1102f88e6ffa532c8c660dde60149157f32d
endormi <39559256+endormi@users.noreply.github.com>
2023-06-07 19:07:11 +0300

    Code restructuring and optimization

--- networking/ip_pinging.py
+++ networking/ip_pinging.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python3
 
 import subprocess
-import time
 import sys
+import time
 
-if len(sys.argv) == 2:
-    sys.path.append("./tools")
-    from logos import Logo
-    import catch_exception as ce
 
-    Logo('IP Pinging')
+def get_input(prompt):
+    if len(sys.argv) == 2:
+        import catch_exception as ce
+        ce.__input__(prompt)
+    return input(prompt)
 
-time.sleep(1)
 
-file = open('/tmp/ip_ping_results.txt', 'w')
+def main():
+    file = open('/tmp/ip_ping_results.txt', 'w')
 
-print("NOTE:")
-print("While this script is legal, this still can be used with malicious intent.")
-print("Don't use this code with malicious intent!")
-print("This is meant for good purposes, DO NOT use this with malicious intent. DO NOT be a criminal.\n")
-print("Only PING IPs that belong to you and the people you have gained permission from!\n")
+    print("NOTE:")
+    print("While this script is legal, this still can be used with malicious intent.")
+    print("Don't use this code with malicious intent!")
+    print("This is meant for good purposes, DO NOT use this with malicious intent. DO NOT be a criminal.\n")
+    print("Only PING IPs that belong to you and the people you have gained permission from!\n")
 
-net = input('Enter network e.g. [192.168.1.1]: ')[0:-1]
-if len(sys.argv) == 2: ce.__input__(net)
+    net = get_input('Enter network e.g. [192.168.1.1]: ')[0:-1]
+    start = get_input('Starting IP: ')
+    end = get_input('Ending IP: ')
 
-start = input('Starting IP: ')
-if len(sys.argv) == 2: ce.__input__(start)
+    print()
 
-end = input('Ending IP: ')
-if len(sys.argv) == 2: ce.__input__(end)
+    for i in range(int(start), int(end)):
+        try:
+            subprocess.check_call(['ping', '-c', '1', net + str(i)], stdout=file, stderr=file)
+        except (subprocess.CalledProcessError, OSError):
+            print(str(net) + str(i) + " Down!")
+        else:
+            print(str(net) + str(i) + " Up!")
 
-print()
 
-for i in range(int(start), int(end)):
-    try:
-        subprocess.check_call(['ping', '-c', '1', net + str(i)], stdout=file, stderr=file)
-    except (subprocess.CalledProcessError, OSError):
-        print(str(net) + str(i) + " Down!")
-    else:
-        print(str(net) + str(i) + " Up!")
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        sys.path.append("./tools")
+        from logos import Logo
+
+        Logo('IP Pinging')
+
+    time.sleep(1)
+    main()
--- networking/server_pinging.py
+++ networking/server_pinging.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
 import os
-import time
 import sys
-
-if len(sys.argv) == 2:
-    sys.path.append("./tools")
-    from logos import Logo
-    import catch_exception as ce
-
-    Logo('Server Pinging')
-
-time.sleep(1)
-
-hostname = input("Domain: ")
-if len(sys.argv) == 2: ce.__input__(hostname)
-
-response = os.system("ping -c 1 " + hostname)
+import time
 
 
-if response == 0:
-    print(hostname + " is up!")
-else:
-    print(hostname + " is down!")
+def get_input(prompt):
+    if len(sys.argv) == 2:
+        import catch_exception as ce
+        ce.__input__(prompt)
+    return input(prompt)
+
+
+def main():
+    hostname = get_input("Domain: ")
+    response = os.system("ping -c 1 " + hostname)
+
+    if response == 0:
+        print(hostname + " is up!")
+    else:
+        print(hostname + " is down!")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        sys.path.append("./tools")
+        from logos import Logo
+
+        Logo('Server Pinging')
+
+    time.sleep(1)
+    main()
